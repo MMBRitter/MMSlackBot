ea1691afd92ea89e5a8ea96186744903712688ab
MMBRitter <45178597+MMBRitter@users.noreply.github.com>
2018-12-10 16:55:15 -0700

    initial commit

---
+++ checkMKapi.js
@@ -0,0 +1,27 @@
+var request = require("request-promise");
+
+module.exports.cpu = function(host){
+	var options = {
+		url: "https://temp.managedmethodsdev.com/monitoring/check_mk/view.py?filled_in=filter&host_regex=" + host + "&service=CPU%20utilization&site=monitoring&view_name=cpusage&output_format=json&_username=automation&_secret=####################" ,
+		method: "GET",
+		rejectUnauthorized: false, 
+	};
+	var rq = request(options)
+		.then(function (body){;
+			return body;
+		});	
+	return rq;
+};
+
+module.exports.mem = function(host){
+	var options = {
+		url: "https://temp.managedmethodsdev.com/monitoring/check_mk/view.py?filled_in=filter&host_regex=" + host + "&service=Memory&site=monitoring&view_name=memusage&output_format=json&_username=automation&_secret=####################" ,
+		method: "GET",
+		rejectUnauthorized: false, 
+	};
+	var rq = request(options)
+		.then(function (body){;
+			return body;
+		});	
+	return rq;
+};
---
+++ index.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+var checkmk = require('./checkMKapi');
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
+
+
+app.post('/mem', function(req, res) {
+	var MEMinstance = req.body.text;
+	console.log("hostname from slack command =" + MEMinstance);
+    checkmk.mem(MEMinstance).then(function(body){
+    	var raw = JSON.parse(body);
+    	var trimmed = raw.slice(1);
+    	var stringed = JSON.stringify(trimmed);
+    	var regexed = stringed.replace(/[\[\]\"]+/g, '');
+    	var linebroken = regexed.replace(/,/g, "\r\n");
+    	console.log(linebroken);
+    	var formatted = {
+    		"response_type": "ephemeral", //change to in_channel for visible to anyone
+    		"text": "Current Memory Usage",
+    		"attachments": [
+    			{
+    				"text":linebroken
+    			}
+    		]
+    	}
+    	res.json(formatted);
+    });
+
+});
+
+app.post('/cpu', function(req, res) {
+	var CPUinstance = req.body.text;
+	console.log(CPUinstance);
+    checkmk.cpu(CPUinstance).then(function(body){
+    	var raw = JSON.parse(body);
+    	var trimmed = raw.slice(1);
+    	var stringed = JSON.stringify(trimmed);
+    	var regexed = stringed.replace(/[\[\]\"]+/g, '');
+    	var linebroken = regexed.replace(/,/g, "\r\n");
+    	console.log(linebroken);
+    	var formatted = {
+    		"response_type": "ephemeral", //change to in_channel to make visible to anyone
+    		"text": "Current CPU Utilization",
+    		"attachments": [
+    			{
+    				"text":linebroken
+    			}
+    		]
+    	}
+    	res.json(formatted);
+    });
+
+});
