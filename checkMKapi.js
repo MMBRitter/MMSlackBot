ae7112f66bc428910ede946c951ecc4c16ff5821
MMBRitter <45178597+MMBRitter@users.noreply.github.com>
2018-12-14 13:12:00 -0700

    channel access/token auth
    Addedd channel specific access logic & slackbot token authentication

--- checkMKapi.js
+++ checkMKapi.js
@@ -2,7 +2,7 @@ var request = require("request-promise");
 
 module.exports.cpu = function(host){
 	var options = {
-		url: "https://temp.managedmethodsdev.com/monitoring/check_mk/view.py?filled_in=filter&host_regex=" + host + "&service=CPU%20utilization&site=monitoring&view_name=cpusage&output_format=json&_username=automation&_secret=####################" ,
+		url: "https://temp.managedmethodsdev.com/monitoring/check_mk/view.py?filled_in=filter&host_regex=" + host + "&service=CPU%20utilization&site=monitoring&view_name=cpusage&output_format=json&_username=automation&_secret=ServiceAccountSecretKey" ,
 		method: "GET",
 		rejectUnauthorized: false, 
 	};
@@ -15,7 +15,7 @@ module.exports.cpu = function(host){
 
 module.exports.mem = function(host){
 	var options = {
-		url: "https://temp.managedmethodsdev.com/monitoring/check_mk/view.py?filled_in=filter&host_regex=" + host + "&service=Memory&site=monitoring&view_name=memusage&output_format=json&_username=automation&_secret=####################" ,
+		url: "https://temp.managedmethodsdev.com/monitoring/check_mk/view.py?filled_in=filter&host_regex=" + host + "&service=Memory&site=monitoring&view_name=memusage&output_format=json&_username=automation&_secret=ServiceAccountSecretKey" ,
 		method: "GET",
 		rejectUnauthorized: false, 
 	};
--- index.js
+++ index.js
@@ -4,15 +4,19 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 var checkmk = require('./checkMKapi');
-
+const unauth = "Unauthorized check, information locked.";
 app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
 
 
 app.post('/mem', function(req, res) {
+    var channel = req.body.channel_id;
+    var token = req.body.token;
 	var MEMinstance = req.body.text;
 	console.log("hostname from slack command =" + MEMinstance);
+
+if ((channel === "channelID") && (token === "slackbotToken")) {
     checkmk.mem(MEMinstance).then(function(body){
     	var raw = JSON.parse(body);
     	var trimmed = raw.slice(1);
@@ -31,12 +35,21 @@ app.post('/mem', function(req, res) {
     	}
     	res.json(formatted);
     });
+}
+else {
+    res.send(unauth);
+}
 
 });
 
 app.post('/cpu', function(req, res) {
+    var channel = req.body.channel_id;
 	var CPUinstance = req.body.text;
+    var token = req.body.token;
+    console.log(token);
 	console.log(CPUinstance);
+
+if ((channel === "channelID") && (token === "slackbotToken")) {
     checkmk.cpu(CPUinstance).then(function(body){
     	var raw = JSON.parse(body);
     	var trimmed = raw.slice(1);
@@ -55,5 +68,8 @@ app.post('/cpu', function(req, res) {
     	}
     	res.json(formatted);
     });
-
+}    
+else {
+    res.send(unauth);
+}
 });
