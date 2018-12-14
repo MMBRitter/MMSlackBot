78ee9e2f42198efe81eba3bbd4506390747b134e
MMBRitter <45178597+MMBRitter@users.noreply.github.com>
2018-12-14 15:17:39 -0700

    formatting

--- checkMKapi.js
+++ checkMKapi.js
@@ -2,12 +2,12 @@ var request = require("request-promise");
 
 module.exports.cpu = function(host){
 	var options = {
-		url: "https://temp.managedmethodsdev.com/monitoring/check_mk/view.py?filled_in=filter&host_regex=" + host + "&service=CPU%20utilization&site=monitoring&view_name=cpusage&output_format=json&_username=automation&_secret=ServiceAccountSecretKey" ,
+		url: "https://temp.managedmethodsdev.com/monitoring/check_mk/view.py?filled_in=filter&host_regex=" + host + "&service=CPU%20utilization&site=monitoring&view_name=cpusage&output_format=json&_username=automation&_secret=ServiceAccountSecretKey",
 		method: "GET",
 		strictSSL: false,
 	};
 	var rq = request(options)
-		.then(function (body){;
+		.then(function (body){
 			return body;
 		});	
 	return rq;
@@ -15,12 +15,12 @@ module.exports.cpu = function(host){
 
 module.exports.mem = function(host){
 	var options = {
-		url: "https://temp.managedmethodsdev.com/monitoring/check_mk/view.py?filled_in=filter&host_regex=" + host + "&service=Memory&site=monitoring&view_name=memusage&output_format=json&_username=automation&_secret=ServiceAccountSecretKey" ,
+		url: "https://temp.managedmethodsdev.com/monitoring/check_mk/view.py?filled_in=filter&host_regex=" + host + "&service=Memory&site=monitoring&view_name=memusage&output_format=json&_username=automation&_secret=ServiceAccountSecretKey",
 		method: "GET",
 		strictSSL: false,
 	};
 	var rq = request(options)
-		.then(function (body){;
+		.then(function (body){
 			return body;
 		});	
 	return rq;
